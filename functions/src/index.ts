ba163b56d8b672205faf6e2b07bfcb19c6fef8e0
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2024-12-12 17:26:34 +0600

    Firebase functions error resolving

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,6 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-
 export const helloWorld = onRequest((request, response) => {
   logger.info("", { structuredData: true });
   response.send("");
